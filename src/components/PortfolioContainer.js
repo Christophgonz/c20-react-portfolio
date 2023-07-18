2c17ceda92eab6c244c48261fa9f5aaeb8a5023a
cgonz <christoph.gonz@gmail.com>
2023-07-18 06:11:06 -0700

    added references to resume

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Home from "./pages/Home";
+import Resume from "./pages/Resume";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
@@ -7,12 +7,12 @@ import Footer from "./Footer";
 import Header from "./Header";
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState("Home");
+  const [currentPage, setCurrentPage] = useState("About");
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
-    if (currentPage === "Home") {
-      return <Home />;
+    if (currentPage === "Resume") {
+      return <Resume />;
     }
     if (currentPage === "About") {
       return <About />;
@@ -30,10 +30,10 @@ export default function PortfolioContainer() {
       {/* We are passing the currentPage from state and the function to update it */}
       <Header currentPage={currentPage} handlePageChange={handlePageChange} />
       {/* Here we are calling the renderPage method which will return a component  */}
-      <body className="container">
+      <div className="container">
         {renderPage()}
         <Footer />
-      </body>
+      </div>
     </div>
   );
 }
