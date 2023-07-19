7d0993d5228bfebbe3725776ea73234021ddb1db
cgonz <christoph.gonz@gmail.com>
2023-07-18 23:46:41 -0700

    added links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,13 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="text-center">
-      <p>This is the footer</p>
+    <footer className="text-center row">
+      <a className="col" href="https://github.com/Christophgonz">
+        GitHub
+      </a>
+      <a className="col" href="#">
+        LinkedIn
+      </a>
     </footer>
   );
 }
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -3,8 +3,9 @@ import React from "react";
 export default function Resume() {
   return (
     <div>
-      <h1>Home Page</h1>
-      <p></p>
+      <h1>Resume</h1>
+      <p>The resume can be viewed and downloaded here:</p>
+      <button>Download</button>
     </div>
   );
 }
