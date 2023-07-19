b5fa482ea297105b5c1866ea65465567344975a1
cgonz <christoph.gonz@gmail.com>
2023-07-18 22:20:29 -0700

    adding projects to portfolio

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function Project(props) {
-  return <div></div>;
+export default function Project({ link, image, name, title }) {
+  return (
+    <div className="col-lg-4 col-md-6 col-sm-12 justify-content-center">
+      <a href={link}>
+        <img src={image} alt={name} />
+        <h3>{title}</h3>
+      </a>
+    </div>
+  );
 }
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,10 +1,52 @@
 import React from "react";
+import Project from "../Project";
 
+const examples = [
+  {
+    link: "https://github.com/Christophgonz/c20-react-portfolio",
+    image: "http://placekitten.com/g/200/300",
+    name: "Test",
+    title: "This is a test",
+    id: "1asdf",
+  },
+  {
+    link: "https://github.com/Christophgonz/c20-react-portfolio",
+    image: "http://placekitten.com/g/200/301",
+    name: "Project 2",
+    title: "This is project 2",
+    id: "2asdf",
+  },
+  {
+    link: "https://github.com/Christophgonz/c20-react-portfolio",
+    image: "http://placekitten.com/g/200/302",
+    name: "",
+    title: "This is a test for a long string of text that will wrap",
+    id: "3asdf",
+  },
+  {
+    link: "https://github.com/Christophgonz/c20-react-portfolio",
+    image: "http://placekitten.com/g/200/302",
+    name: "",
+    title: "This is a test for a long string of text that will wrap",
+    id: "4asdf",
+  },
+  {
+    link: "https://github.com/Christophgonz/c20-react-portfolio",
+    image: "http://placekitten.com/g/200/302",
+    name: "",
+    title: "This is a test for a long string of text that will wrap",
+    id: "5asdf",
+  },
+];
 export default function Portfolio() {
   return (
     <div>
-      <h1>Blog Page</h1>
-      <p></p>
+      <h1>Portfolio</h1>
+      <div className="row text-center">
+        {examples.map((example) => (
+          <Project key={example.id} {...example} />
+        ))}
+      </div>
     </div>
   );
 }
